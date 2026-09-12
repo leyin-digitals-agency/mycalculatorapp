48f6b7a0b51cb6090b05ec6d02ad4048710ffa4a
kelechi <kelechi@tribesquare.co>
2026-09-12 17:36:07 +0100

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
 
-    // class = className
+  // class = className
     // onclick = onClick
     // style="" = style={{}}
 
-    // Start your server with npm run dev and open the link shown in the terminal to view it in the browser.
+    // Start your server with 
+    // npm run dev 
+    // and open the link shown in the terminal to view it in the browser.
 
     // To push to your github repository, you can use the following commands in your terminal:
     // git add .
@@ -17,12 +18,34 @@ function App() {
     // To pull changes from your github repository, you can use the following command in your terminal:
     // git pull origin main
 
+function App() {
+
+    const [displayValue, setDisplayValue] = useState(0);
+
+
+    function displayNumber(number) {
+        setDisplayValue(number);
+    }
+
+    const displayNumber2 = function(number) {
+        setDisplayValue(number);
+    }
+
+    const arrowFunctions = (number) => {
+        setDisplayValue(number);
+    }
+
+    //assignment 1 = What is an arrow function?
+    //2: difference between an arrow function and a regular function
+    // 3: Instead of replacing the display value, make it so that the number is added to the current display value. For example, if the current display value is 1 and you click on 2, the display value should become 12. If you click on 3 after that, the display value should become 123.
+
+
 
     return (
         <div className="calculator">
             {/* Make title color white */}
             <p className="title">Leyin's Calculator</p> 
-            <input type="text" id="display" readOnly />
+            <input type="text" id="display" readOnly value={displayValue} />
 
             <div className="buttons">
                 <button className="clear">C</button>
@@ -40,9 +63,9 @@ function App() {
                 <button >6</button>
                 <button >.</button>
 
-                <button >1</button>
-                <button >2</button>
-                <button >3</button>
+                <button onClick={() => setDisplayValue(1)}>1</button>
+                <button onClick={() => setDisplayValue(2)}>2</button>
+                <button onClick={() => setDisplayValue(3)}>3</button>
                 <button className="equal" >=</button>
 
                 <button style={{ gridColumn: "span 4" }}  >0</button>
